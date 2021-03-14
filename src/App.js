db25a0b1d972df14bc6a49077661bb2a95b33df3
PhatTranSon <phatranson1@gmail.com>
2021-03-13 19:19:54 -0800

    Added sliding panel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Background from './components/background';
 import Button from './components/button';
+import SlidingPanel from './components/slidingpanel';
 import GlobalStyle from './components/styles/global';
 import { Heading, SubHeading } from "./components/typography";
 
+import { CloseIcon } from "./components/icon";
+
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  function openPanel() {
+    setOpen(true);
+  }
+
+  function closePanel() {
+    setOpen(false);
+  }
+
   return (
     <React.Fragment>
       <GlobalStyle/>
@@ -12,10 +25,22 @@ function App() {
         <Heading>
           Welcome to TrainIT
         </Heading>
+
         <SubHeading>
           Tools for training, monitoring and deploying Machine Learning models
         </SubHeading>
-        <Button>Create project</Button>
+
+        <Button onClick={() => openPanel()}>Create project</Button>
+
+        <SlidingPanel
+          open={open}
+          onClose={() => closePanel()}>
+          <div>
+            <CloseIcon
+              onClick={() => closePanel()}/>
+            Create project
+          </div>
+        </SlidingPanel>
       </Background>
     </React.Fragment>
   );
